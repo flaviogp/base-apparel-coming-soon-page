e4348d778f4866982257c10691ed8786a35f7ca5
FlavioGP <flavio.gpinheiro@hotmail.com>
2025-02-04 10:33:09 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="bg-black">
+      <p className="text-white">Olá</p>
+      <span className="text-yellow">Mundo!</span>
+    </div>
+  );
+}
+
+export default App;
